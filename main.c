97c96ea006dd1416de25b7b218d55f13ca2379f7
vitket <skolkojvastut@gmail.com>
2018-01-22 19:57:12 +0200

    hh

---
+++ main.c
@@ -0,0 +1,31 @@
+//
+//  main.c
+//  1 - 1000 000
+//
+//  Created by vit on 21/1/18.
+//  Copyright © 2018 Jimmi Morrisson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int array[1000] = {0};
+    int flag, walk, sum, get_ch, sum2, i;
+    printf(":");
+
+    for(walk=0; (flag=getchar())!='\n'; walk++)
+            array[walk] = flag;
+
+    array[walk] = '\n';
+
+    for(walk=0,sum=0; (get_ch=array[walk])!='\n'; ++walk)
+        sum += get_ch-48;
+
+    printf("sum of input integers: %d\n\n", sum);
+    
+//    for(i=1,sum2=0; i<10; i++)
+//        sum2 += (20*i);
+//    printf("%d\n", sum2+1);
+    
+}
